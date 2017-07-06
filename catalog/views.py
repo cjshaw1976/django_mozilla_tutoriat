99a43223af9609dc7ac08df36da63fb8de68d921
Chris Shaw <chris@shawcando.com>
2017-07-06 08:10:33 +0200

    Added visit count using session

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.views import generic
 
@@ -7,19 +9,29 @@ def index(request):
     """
     View function for home page of site.
     """
-    # Generate counts of some of the main objects
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
-    # Available books (status = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()
     num_genre=Genre.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    visit={'num':num_visits,
+           'last':request.session.get('last_visit', '')}
+    request.session['num_visits'] = num_visits+1
+    request.session['last_visit'] = datetime.datetime.now().strftime("%d %b %Y %H:%M")
 
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre},
+        context={'num_books':num_books,
+                 'num_instances':num_instances,
+                 'num_instances_available':num_instances_available,
+                 'num_authors':num_authors,
+                 'num_genre':num_genre,
+                 'visit':visit},
     )
 
 
